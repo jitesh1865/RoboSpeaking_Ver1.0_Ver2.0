d6adb0d152c122cf624b5f4e9d2d90fb6a4ea185
jitesh sharma <you@example.com>
2024-06-06 18:39:51 +0530

    Version 1.0

---
+++ RoboSpeaker_Version1.0_Project.py
@@ -0,0 +1,79 @@
+
+#import gtts
+from playsound import playsound
+# This module is imported so that we can 
+# play the converted audio
+
+
+from gtts import gTTS
+
+import os
+import pyttsx3
+
+
+
+
+# //---------------Code is Good and in a Working condition-----Code Start A1-------------------------------
+import pyttsx3
+
+while 1:
+        print("-------------Welcome to Robo speaker 1.0 Version---------------")
+        eng=pyttsx3.init()
+        c=input("Enter a text by user : \n")
+        if c=='q':
+                eng.say("Now Good BYe BYE and will meet next time")
+                vw=eng.getProperty("voices")
+                eng.runAndWait()
+                break
+        eng.say(c)
+        v=eng.getProperty("voices")
+        eng.runAndWait()
+# //------------------------------Code End A1--------------------------------------------------------------
+
+
+
+
+
+'''
+
+while 1:  
+        print("----------------Welcome to Robo Speaker! Lets Have a Fun------------------------")
+        inputTxt=input("Enter a text message that user want to speak : \n")
+        if inputTxt=='q':
+                       break
+        sound=gtts.gTTS(inputTxt,lang="en")
+        sound.save("Jit.mp3")
+        playsound.playsound("Jit.mp3")
+
+
+'''
+
+
+
+'''
+from pathlib import Path
+from openai import OpenAI
+
+client = OpenAI()
+
+sfp=Path(__file__).parent
+res=client.audio.speech.create(model="tts-1", voice="alloy",input="How are you man")
+
+res.stream_to_file("sfp.mp3")
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RoboSpeaking_Version2.0_Project.py
@@ -0,0 +1,41 @@
+import multiprocessing
+from gtts import gTTS
+from playsound import playsound
+import os
+
+'''
+        print("----------------Welcome to Robo Speaker! Lets Have a Fun------------------------")
+        inputTxt=input("Enter a text message that user want to speak : \n")
+        if inputTxt=='q':
+                       break
+        sound=gtts.gTTS(inputTxt,lang="en")
+        sound.save("Jit.mp3")
+        playsound.playsound("Jit.mp3")
+'''
+while 1:
+        print("----------------Welcome to Robo Speaker  Version 2.0 Improved Version! Lets Have a Fun-----------------------")
+        Usertext=input("Enter a Text by user : ")
+        var = gTTS(text = Usertext,lang = 'en')
+        if Usertext=='q':
+                    exitText="Ok Bye Bye Chat End Now , will meet Next Time "
+                    var = gTTS(text = exitText,lang = 'en')
+                    var.save('engw.mp3') 
+                    playsound('engw.mp3')
+                    break
+        var.save('eng.mp3') 
+        playsound('eng.mp3')
+        os.remove('eng.mp3')
+        
+
+
+
+
+
+
+
+
+
+
+#//----------For Hindi Language speaking Robo-------------------
+#var=gTTS(text="यह हिंदी में एक उदाहरण है",lang='hi')
+#//--------------------------------------------------------------
